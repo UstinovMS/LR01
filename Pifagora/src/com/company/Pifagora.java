5b912dec376106133b79cff6e68e64361b468011
UstinovMS <73177764+UstinovMS@users.noreply.github.com>
2022-01-21 03:58:20 +0300

    Laba01

---
+++ Pifagora/src/com/company/Pifagora.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Pifagora {
+    public static void main(String[] args) {
+        double a = 3;
+        double b = 4;
+        double c;
+        c = Math.sqrt (a* a + b* b);
+        System.out.println ("c = "+ c);
+    }
+}
